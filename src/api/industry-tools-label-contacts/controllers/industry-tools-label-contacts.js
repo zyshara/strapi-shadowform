b8da3969ca4573b75c749daa82c8dfaa190a912b
zyshara <hi.imma.bear@gmail.com>
2022-10-06 23:27:00 -0700

    feat: industry tools label contacts

---
+++ src/api/industry-tools-label-contacts/controllers/industry-tools-label-contacts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * industry-tools-label-contacts controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::industry-tools-label-contacts.industry-tools-label-contacts');
---
+++ src/api/industry-tools-label-contacts/services/industry-tools-label-contacts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * industry-tools-label-contacts service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::industry-tools-label-contacts.industry-tools-label-contacts');
