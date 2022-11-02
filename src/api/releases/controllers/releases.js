ab79a341adcaf8c8b365062cd1095f513eb4a64a
zyshara <hi.imma.bear@gmail.com>
2022-11-02 15:51:37 -0700

    feat: remove release model

--- src/api/releases/controllers/releases.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * releases controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::releases.releases');
--- src/api/releases/services/releases.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * releases service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::releases.releases');
