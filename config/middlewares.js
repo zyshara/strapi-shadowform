117c615d7357896db14d16706f318c1dd6d77e25
zyshara <hi.imma.bear@gmail.com>
2024-03-22 01:40:09 -0700

    feat: google cloud platform upload

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,24 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = {
+   upload: {
+      config: {
+         provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+         providerOptions: {
+            serviceAccount: env.json('GCS_SERVICE_ACCOUNT'), 
+            bucketName: env('GCS_BUCKET_NAME'),
+            publicFiles: true,
+         },
+      },
+   },
+}
